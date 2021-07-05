7c1b9b042be2d1624e3fbca75a28aa75c373f6f6
Apig Sharbo <apig.sharbo@wmg.com>
2021-07-05 18:55:28 +0400

    added swagger-ui stuff

--- lib/cdk-swagger-ui-stack.ts
+++ lib/cdk-swagger-ui-stack.ts
@@ -1,9 +1,44 @@
 import * as cdk from '@aws-cdk/core';
+import * as S3 from "@aws-cdk/aws-s3";
+import * as S3Deployment from "@aws-cdk/aws-s3-deployment";
+import * as fs from "fs";
 
 export class CdkSwaggerUiStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+
+    const bucket = new S3.Bucket(this, "Files", {
+      websiteIndexDocument: "index.html",
+      publicReadAccess: true,
+    });
+
+    const path = "./swagger-ui/dist/"
+    const filePath = path + "index.html"
+    // Do replacement in file
+    fs.readFile(filePath, 'utf8', function (err,data) {
+      if (err) {
+        return console.log(err);
+      }
+
+      // Actual thing i want to do
+      // const result = data.replace(/https:\/\/.\+.json/, 'my.json');
+
+      // For testing with no changes
+      const result = data
+
+      fs.writeFile(filePath, result, 'utf8', function (err) {
+        if (err) return console.log(err);
+      });
+    });
+
+    new S3Deployment.BucketDeployment(this, 'Deployment', {
+      sources: [S3Deployment.Source.asset(path)],
+      destinationBucket: bucket,
+    });
+
+    new cdk.CfnOutput(this, 'BucketDomain', {
+      value: bucket.bucketWebsiteDomainName,
+    });
   }
 }
